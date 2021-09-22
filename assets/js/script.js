575151c8e53b763834c427e4935fec24f84eaa07
Marcus Rothoff <marcusrothoff@hotmail.com>
2021-09-22 16:04:43 +0200

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementScoreWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
