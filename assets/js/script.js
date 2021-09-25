008ab0305d3a2425cef0a8e80e1483f8b36328d8
Marcus Rothoff <marcusrothoff@hotmail.com>
2021-09-25 13:22:25 +0200

    add division to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,9 @@ function runGame(gameType) {
 
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    }else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -81,7 +83,9 @@ if(operator === "+") {
     return [operand1 * operand2, "multiply"]
 } else if (operator === "-") {
     return [operand1 - operand2, "subtract"]
-}else {
+} else if (operator === "/") {
+    return [operand1 / operand2, "division"]
+} else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}.Aborting!`;
 }
@@ -121,4 +125,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = (operand1 * operand2); 
+    document.getElementById('operand2').textContent = operand2; 
+    document.getElementById('operator').textContent = "/";
 }
